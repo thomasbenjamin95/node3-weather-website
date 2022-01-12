fe5798100605b68ae537b11a12f97bae7b34f238
Thomas Benjamin <thomasbenjamin95@protonmail.com>
2022-01-12 12:47:51 -0500

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,7 @@ messageTwo.textContent = ''
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
-
     const location = search.value  
-
     fetch('/weather?address=' +location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
@@ -23,3 +21,44 @@ weatherForm.addEventListener('submit', (e)=>{
 })
 })
 
+document.getElementById('button-toronto').addEventListener("click", ()=>{
+    const location = 'Toronto'
+    fetch('/weather?address=' +location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+
+    document.getElementById('button-newyork').addEventListener("click", ()=>{
+        const location = 'New York'
+        fetch('/weather?address=' +location).then((response) => {
+            response.json().then((data) => {
+                if(data.error) {
+                    messageOne.textContent = data.error
+                } else{
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+            })
+        })
+})
+
+document.getElementById('button-amsterdam').addEventListener("click", ()=>{
+    const location = 'Amsterdam'
+    fetch('/weather?address=' +location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
+})
--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.get('/about', (req, res)=>{
 app.get('/help', (req, res)=>{
     res.render('help',{
         title: 'Help',
-        helpText: 'This is some helpful text',
+        helpText: 'Feel free to connect me on LinkedIn',
         name: 'Thomas'
     })
 })
